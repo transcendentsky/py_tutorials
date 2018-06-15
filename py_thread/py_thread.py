65730ff2d1da498e9ad4a748b971e3e3bb525abb
trans <transcendentsky@users.noreply.github.com>
2018-06-15 12:35:28 +0800

    add some

---
+++ py_thread/py_thread.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+import yaml
+import numpy as np
+import sys
+import os
+import subprocess
+try:
+    import thread
+except:
+    import _thread as thread
+import time
+
+
+def printwocao(o=None):
+    i = 0
+    while i < 11:
+        print('wocao? {}'.format(o))
+        time.sleep(1)
+        i += 1
+
+thread.start_new_thread(printwocao,())
+
+# 这个十分重要， 当进程结束时，子线程也会结束
+while 1:
+    pass
---
+++ py_thread/thread_communication.py
@@ -0,0 +1,42 @@
+#coding:utf-8
+from queue import Queue
+from threading import Thread
+import time
+
+# A thread that produces data
+def producer(out_q):
+    while True:
+        # Produce some data
+        # ...
+        data = 'a str'
+        out_q.put(data)
+
+# A thread that consumes data
+def consumer(in_q):
+    while True:
+        # Get some data
+        data = in_q.get()
+        print(data)
+        # Process the data
+        ...
+
+# Create the shared queue and launch both threads
+q = Queue()
+# t1 = Thread(target=consumer, args=(q,))
+# t2 = Thread(target=producer, args=(q,))
+# t1.start()
+# t2.start()
+
+
+def printer():
+    for _ in range(12):
+        time.sleep(1)
+        print("testing...")
+    print('over...')
+
+t3 = Thread(target=printer, args=())
+t3.start()    # 线程开始
+time.sleep(2)
+print("start join")
+t3.join()     # 等待程序完成
+print('Join Over..')
