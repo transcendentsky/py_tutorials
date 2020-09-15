ffda3e9e249cc87f5205a88f5960806912198f18
Popper <transcendentsky@gmail.com>
2020-09-15 18:40:41 +0800

    add medical code

---
+++ medical/nrrd2nii.py
@@ -0,0 +1,19 @@
+import os
+from glob import glob
+import nrrd #pip install pynrrd, if pynrrd is not already installed
+import nibabel as nib #pip install nibabel, if nibabel is not already installed
+import numpy as np
+
+baseDir = os.path.normpath('path/to/file')
+files = glob(baseDir+'/*.nrrd')
+
+for file in files:
+#load nrrd
+  _nrrd = nrrd.read(file)
+  data = _nrrd[0]
+  header = _nrrd[1]
+
+#save nifti
+  img = nib.Nifti1Image(data, np.eye(4))
+  nib.save(img,os.path.join(baseDir, file[-8:-5] + '.nii.gz'))
+
